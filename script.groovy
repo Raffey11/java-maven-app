1753a5880f2e4ab567d3ba5829bf22aaba9e3456
Raffey11 <41152148+Raffey11@users.noreply.github.com>
2023-12-09 08:31:16 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t raffey/java-maven-app:1.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push raffey/java-maven-app:1.1'
