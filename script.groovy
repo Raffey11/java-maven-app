f8b8d75cd89dbf3e8fd251cf29c508950d8212f0
Raffey Nasser <raffaynasser@gmail.com>
2023-12-12 10:03:10 +0500

    changed script.groovy file

--- script.groovy
+++ script.groovy
@@ -15,8 +15,8 @@ def buildImage() {
 def deployApp() {
     def shellCmd = 'bash ./shell-cmds.sh'
     sshagent(['ec2-server-key']) {
-        sh "scp shell-cmds.sh ec2-user@34.245.238.143:~"
-        sh "scp docker-compose.yaml ec2-user@34.245.238.143:~"
+        sh "scp -o StrictHostKeyChecking=no shell-cmds.sh ec2-user@34.245.238.143:~"
+        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ec2-user@34.245.238.143:~"
         sh "ssh -o StrictHostKeyChecking=no ec2-user@34.245.238.143 ${shellCmd}"
     }
 } 
